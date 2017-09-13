b0788b3412d924217d213f1e524a20954e9c592a
bord81 <bslabut@gmail.com>
2017-09-13 14:20:43 +0300

    Initial commit

---
+++ app/src/test/java/labut/md311/texter/ExampleUnitTest.java
@@ -0,0 +1,18 @@
+package labut.md311.texter;
+
+import android.test.ActivityInstrumentationTestCase;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
